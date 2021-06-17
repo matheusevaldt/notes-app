c89d660e1a2944886cde6ba872f73de7aec811a4
Matheus Evaldt <evaldtmatheus@gmail.com>
2021-06-17 01:09:34 -0300

    Working in the expand/collapse features in the 'Note' component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../Note.css';
 
 const Note = ({ title, message, priority, noteCreation, lastUpdated, note, notes, setNotes, amountOfNotes, setAmountOfNotes, setFormIsOpened, noteEdited, setNoteEdited, noteIsBeingEdited, setNoteIsBeingEdited }) => {
@@ -31,15 +31,55 @@ const Note = ({ title, message, priority, noteCreation, lastUpdated, note, notes
         setFormIsOpened(true);
     }
 
+    const handleExpandButton = (event) => {
+        const element = event.target.parentElement.parentElement.firstChild;
+        console.log(element);
+        element.classList.remove('message-blur');
+        element.nextSibling.firstChild.style.display = 'none'; // Expand button
+        element.nextSibling.firstChild.nextSibling.style.display = 'block'; // Collapse button
+    };
+
+    const handleCollapseButton = (event) => {
+        const element = event.target.parentElement.parentElement.firstChild;
+        console.log(element);
+        element.classList.add('message-blur');
+        element.nextSibling.firstChild.style.display = 'block';
+        element.nextSibling.firstChild.nextSibling.style.display = 'none';
+    };
+
+    useEffect(() => {
+        const element = document.getElementById(`${note.id}`);
+        console.log(message);
+        console.log(`HEIGHT: ${element.clientHeight}`);
+        if (element.clientHeight > 72) {
+            element.classList.add('message-blur');
+            element.nextSibling.classList.add('display-buttons-message');
+            console.log(element.nextSibling);
+            // Reset buttons. Show Expand and Hide Collapse
+            element.nextSibling.firstChild.style.display = 'block';
+            element.nextSibling.firstChild.nextSibling.style.display = 'none';
+        } else {
+            element.nextSibling.classList.remove('display-buttons-message');
+            element.classList.remove('message-blur');
+            // element.style.height = 'auto';
+        }
+    }, [message, note.id]);
+
     return (
-        <div className='note-container' >
+        <div className='note-container'>
             <div className='note-header'>
                 <h2>{title}</h2>
                 <button onClick={handleDeleteNote} className='button-delete-note'>&times;</button>
             </div>
             <p className='note-priority'><strong>{priority}</strong> priority</p>
             <div className='note-main'>
-                <li className={`note-message ${note.completed ? 'note-completed' : ''}`}>{message}</li>
+                <li className='note-message' id={note.id}>
+                    {message}
+                </li>
+                <div className='buttons-message'>
+                    <button onClick={(e) => handleExpandButton(e)} className='button-expand'>EXPAND</button>
+                    <button onClick={(e) => handleCollapseButton(e)} className='button-collapse'>COLLAPSE</button>
+                </div>
             </div>
             <div className='note-footer'>
                 <button onClick={openEditNote} className='button-open-edit-note'>EDIT NOTE</button>
@@ -48,11 +88,6 @@ const Note = ({ title, message, priority, noteCreation, lastUpdated, note, notes
                     <p className={`last-updated ${lastUpdated !== '' ? 'display-last-updated': ''}`}>Last updated on {lastUpdated}</p>
                 </div>
             </div>
-            
-            
-            {/* <button onClick={handleStatusNote} className='button-status-note'>V</button> */}
-            
-            
         </div>
     );
 };
